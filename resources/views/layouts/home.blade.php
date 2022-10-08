4e6c90550044fcf2f4f68e52e0b01b12aeaf83a5
BENJAH <benjahjp@gmail.com>
2022-10-08 16:01:46 +0300

    edit

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -273,14 +273,15 @@
             <div id="page-content-wrapper">
                 <nav class="navbar sticky-top shadow-sm navbar-expand-md navbar-light bg-white mr-auto">
                     <div class="container">
-
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            {{ __('School Name') }}
+                        </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                             <span class="navbar-toggler-icon"></span>
                         </button>
 
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <!-- Left Side Of Navbar -->
-                            
 
                             <!-- Right Side Of Navbar -->
                             <ul class="navbar-nav ms-left">
